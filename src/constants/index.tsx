42a7384e18ded2089e559e3c9ced4183076c918a
PriyanshGupta2002 <guptapriyansh905@gmail.com>
2026-01-18 00:22:52 +0530

    refine setup, pages routing and datatable setup

---
+++ src/constants/index.tsx
@@ -0,0 +1,5 @@
+export const departments = ["CS", "Math", "English"];
+export const DEPARTMENT_OPTIONS = departments.map((dep) => ({
+  label: dep,
+  value: dep,
+}));
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type subject = {
+  id: number;
+  name: string;
+  description: string;
+  department: string;
+  createdAt: string;
+};
